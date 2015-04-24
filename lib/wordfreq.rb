c0d773e5abfa3b71e6de6845addf8ddd22530653
Dan <student@epicodus.com>
2015-04-24 09:52:40 -0700

    Added require(pry) for troubleshooting purposes. Wrote a spec to check that the method will return 0 if the object doesn't match the argument.

--- lib/wordfreq.rb
+++ lib/wordfreq.rb
@@ -3,7 +3,7 @@ class String #This method will take a word (self) and a sentence (check)
   define_method(:wordfreq) do |check|
     freq = 0 #variable for holding the frequency of the word, which is eventually returned
     #letters = self.split("") #splits the word that's to be checked for frequency into an array of letters
-    sent_words = self.split(" ") #splits the sentence that is being checked into an array of words
+    sent_words = check.split(" ") #splits the sentence that is being checked into an array of words
     sent_words.each do |word| #loops through every word of the sentence
         if word.==self
           freq = 1 #sets freq to 1 to pass first spec
--- spec/wordfreq_spec.rb
+++ spec/wordfreq_spec.rb
@@ -1,8 +1,13 @@
 require('rspec')
 require('wordfreq')
+require('pry')
 
 describe('String#wordfreq') do
   it("It will return 1 if one word matches another word") do
     expect("test".wordfreq("test")).to(eq(1))
+  end
+
+  it("It will return 0 if a word does not match another word") do
+    expect("test".wordfreq("bog")).to(eq(0))
   end
 end
