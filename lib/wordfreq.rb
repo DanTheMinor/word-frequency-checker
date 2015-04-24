de87e48097245184862715ef0d05eed0b6933424
Dan <student@epicodus.com>
2015-04-24 09:42:03 -0700

    First spec passed. The method will check if a word is contained in a sentence, and if it is, return 1, otherwise return 0.

--- lib/wordfreq.rb
+++ lib/wordfreq.rb
@@ -1,5 +1,14 @@
 class String #This method will take a word (self) and a sentence (check)
   # and it will return the frequency of the word in that sentence.
-  define_method(word_freq) do |check|
+  define_method(:wordfreq) do |check|
+    freq = 0 #variable for holding the frequency of the word, which is eventually returned
+    #letters = self.split("") #splits the word that's to be checked for frequency into an array of letters
+    sent_words = self.split(" ") #splits the sentence that is being checked into an array of words
+    sent_words.each do |word| #loops through every word of the sentence
+        if word.==self
+          freq = 1 #sets freq to 1 to pass first spec
+        end
+    end
+    freq
   end
 end
--- spec/wordfreq_spec.rb
+++ spec/wordfreq_spec.rb
@@ -1,8 +1,8 @@
 require('rspec')
-require('anagrams')
+require('wordfreq')
 
-describe('String#word_freq') do
+describe('String#wordfreq') do
   it("It will return 1 if one word matches another word") do
-    expect("test".word_freq("test")).to(eq(1))
+    expect("test".wordfreq("test")).to(eq(1))
   end
 end
