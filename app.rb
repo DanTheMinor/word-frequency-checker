0f0880eded3ac038a8d095f20af0c4a80f15c954
Dan <student@epicodus.com>
2015-04-24 10:38:12 -0700

    Included readme file, updated app.rb to fetch parameters from the form on index.

--- app.rb
+++ app.rb
@@ -8,6 +8,10 @@ get('/') do
 end
 
 get('/wordfreq') do
-  #write a params.fetch here
+
+  @word = params.fetch('word') #the word whose frequency is being determined is pulled from the form
+  @string = params.fetch('string') #the sentence being checked for our word is pulled from the form
+  @freq = @word.wordfreq(@string) #the frequency is determined using the wordfreq method
+  binding.pry
   erb(:wordfreq)
 end
